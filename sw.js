0d3b3b15e722cfe1e030c34cb5a46e26adcde5ae
hamidnoor27 <hamidnoor990@gmail.com>
2026-02-21 06:26:10 +0500

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("yaseen-cache-v1").then(cache => cache.addAll(["./"]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
